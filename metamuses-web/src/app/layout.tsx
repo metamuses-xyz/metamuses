65e231f1ad843fc5d33c7c207d69e7881c46e415
choguun <visaruth.s@gmail.com>
2025-11-25 23:51:07 +0700

    add: web analytic

--- metamuses-web/src/app/layout.tsx
+++ metamuses-web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Web3Provider } from "@/providers/Web3Provider";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Web3Provider>{children}</Web3Provider>
+        <Analytics />
       </body>
     </html>
   );
